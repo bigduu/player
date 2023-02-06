10888aa8e44c649be97bfa53d5e04b9090ce56cf
bigduu <mugeng.du@qq.com>
2023-02-07 01:33:44 +0800

    test windows play video

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,9 @@
 #![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
+
+use std::env;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -9,9 +11,34 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn get_video_path() -> Vec<String> {
+    let exe = env::current_exe().unwrap();
+    let dir = env::current_dir().unwrap();
+    let current_dir = dir.to_str().unwrap();
+    let current_exe = exe.to_str().unwrap();
+    println!("current_dir: {}", current_dir);
+    println!("current_exe: {}", current_exe);
+    let entries = std::fs::read_dir(".").unwrap();
+    let mut paths = Vec::new();
+    for entry in entries {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        let path = path.to_str().unwrap().to_string();
+        println!("path: {}", path);
+        if path.ends_with(".mp4") {
+            paths.push(path);
+        }
+    }
+    paths.push(current_dir.to_string());
+    paths.push(current_exe.to_string());
+    paths
+}
+
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_video_path])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/components/MyPlayer.vue
+++ src/components/MyPlayer.vue
@@ -1,45 +1,76 @@
-<script setup>
-  import { defineComponent,ref,shallowRef,onMounted } from 'vue'
-  import { VideoPlayer } from '@videojs-player/vue'
-
-  defineComponent({
-    components: {
-      VideoPlayer
-    }
-  })
-
-  const windowWidth = ref(window.innerWidth)
-  const windowHeight = ref(window.innerHeight)
-  const player = shallowRef()
-  const state = shallowRef()
-  const videoPlayer = ref(null)
-
-  onMounted(() => {
-      window.addEventListener('resize', updateSize)
-  })
-
-  const updateSize = () => {
-    windowWidth.value = window.innerWidth
-    windowHeight.value = window.innerHeight
-  }
-
-  const video_mounted = (payload) => {
-        state.value = payload.state
-        player.value = payload.player
-  }
-
-
-</script>
-
 <template>
+  <button @click="play">Play</button>
+  <button @click="pause">Pause</button>
+  <button @click="changeVideo">ChangeVideo</button>
   <VideoPlayer
-    ref="videoPlayer"
-    src="test.mp4"
-    @mounted="video_mounted"
-    :width="windowWidth"
-    :height="windowHeight"
-    :loop="true"
-    :controls="true"
-    :options="{autoplay:true,preferFullWindow:true}"
+      ref="videoPlayer"
+      :src="current_video"
+      @mounted="video_mounted"
+      @ended="changeVideo"
+      :width="windowWidth"
+      :height="windowHeight"
+      :options="{autoplay:true,preferFullWindow:true}"
   />
 </template>
+
+<script setup>
+import {defineComponent, onMounted, ref, shallowRef} from 'vue'
+import {VideoPlayer} from '@videojs-player/vue'
+import {BaseDirectory, readDir} from '@tauri-apps/api/fs';
+import { convertFileSrc } from '@tauri-apps/api/tauri';
+import {}  from '@tauri-apps/api/app'
+
+defineComponent({
+  components: {
+    VideoPlayer
+  }
+})
+
+const windowWidth = ref(window.innerWidth)
+const windowHeight = ref(window.innerHeight)
+const player = shallowRef()
+const state = shallowRef()
+const videoPlayer = ref(null)
+const video_path = ref([])
+const video_index = ref(0)
+const current_video = ref('')
+
+onMounted(async () => {
+  window.addEventListener('resize', updateSize)
+  const baseDirPath = BaseDirectory.Desktop;
+  await readDir("", {dir: baseDirPath, recursive: true}).then((files) => {
+    for (const file of files) {
+      if (file.path.includes(".mp4")) {
+        const videoPath = convertFileSrc(file.path)
+        console.log(videoPath)
+        video_path.value.push(videoPath)
+      }
+    }
+  });
+})
+
+const changeVideo = () => {
+  video_index.value = (video_index.value + 1) % video_path.value.length
+  console.log(video_path.value[video_index.value])
+  current_video.value = video_path.value[video_index.value]
+}
+
+const play = () => {
+  player.value.play()
+}
+
+const pause = () => {
+  player.value.pause()
+}
+
+const updateSize = () => {
+  windowWidth.value = window.innerWidth
+  windowHeight.value = window.innerHeight
+}
+
+const video_mounted = (payload) => {
+  state.value = payload.state
+  player.value = payload.player
+}
+</script>
+
